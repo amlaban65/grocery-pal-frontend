70441ef25326454d1c9e436064ff030aa1c52305
amlaban65 <amlaban@sas.upenn.edu>
2022-08-09 01:54:55 -0400

    frontend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 import { useLogin } from '../hooks/useLogin';
 
-const register = async (e) => {
-    e.preventDefault();
-}
 const Login = () => {
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
