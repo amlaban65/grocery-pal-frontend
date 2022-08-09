492e82c5896dfb1890c324b39745a308ad24ac0e
amlaban65 <amlaban@sas.upenn.edu>
2022-08-09 02:00:22 -0400

    fixx

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -9,7 +9,7 @@ export const useSignup = () => {
     const signup = async (email, password) => {
         setLoading(true);
         setError(null);
-        const response = await fetch('https://grocery-pal.herokuapp.com/api/user/signup', {
+        const response = await fetch('/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
